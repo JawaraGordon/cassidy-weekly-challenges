0047ac27e2bf29208bef875ee57f77dc1535b7c1
JawaraGordon <jawaragordon@icloud.com>
2025-01-22 22:55:18 -0500

    added README

--- longestSubsequence.js
+++ longestSubsequence.js
@@ -1,4 +1,5 @@
 // Given a list of integers, write a function that finds the longest subsequence where the difference between consecutive elements is either 1 or -1. Return the length of this subsequence.
+
 // Examples:
 // > longestSubsequence([1,2,3,4,5])
 // 5
@@ -6,7 +7,6 @@
 // 2
 // > longestSubsequence([10,11,7,8,9,12])
 // 3
-
 
 const nums = [1, 2, 3, 2, 5, 6, 7, 8, 9];
 
